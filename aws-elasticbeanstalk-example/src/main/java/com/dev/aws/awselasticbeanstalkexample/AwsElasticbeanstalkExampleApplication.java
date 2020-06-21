cc26985eab537e89390727053bb544d8f305fb90
AnudeepBygari <48363065+AnudeepBygari@users.noreply.github.com>
2020-06-21 10:05:11 +0530

    Add files via upload

---
+++ aws-elasticbeanstalk-example/src/main/java/com/dev/aws/awselasticbeanstalkexample/AwsElasticbeanstalkExampleApplication.java
@@ -0,0 +1,13 @@
+package com.dev.aws.awselasticbeanstalkexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsElasticbeanstalkExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsElasticbeanstalkExampleApplication.class, args);
+	}
+
+}
